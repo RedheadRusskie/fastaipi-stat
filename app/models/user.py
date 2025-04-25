39e739c4bf2564dd3deae243f9c9ec795abc8166
Ivan Konnikov <ivan.liaskin@gmail.com>
2025-04-25 21:59:02 +0200

    Added SQL UNIQUE constraint to user usernames.

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,11 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String, nullable=False)
+    username = Column(
+        String,
+        unique=True,
+        nullable=False,
+    )
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     hashed_password = Column(String, nullable=False)
     datasets = relationship("Dataset", backref="user", cascade="all, delete-orphan")
